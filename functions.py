38e705a6aebb4324626c944a929eeae3969adc9a
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-24 22:42:03 -0700

    lines backward

--- functions.py
+++ functions.py
@@ -1,10 +1,25 @@
-filename = "poem.txt"
+import random
 
 def get_file_lines(filename):
     in_file = open(filename, "r")
-    poem = in_file.read()
-    print(poem)
+    poem = in_file.readlines()
+    # print(poem)
+    return poem
+
+def lines_printed_backwards(lines_list):
+    # get_file_lines('poem.txt')
+    total_lines = len(lines_list) 
+    for line in reversed(lines_list):
+        print(f'{total_lines} {line}')
+        total_lines -= 1
+
+def lines_printed_random(lines_list):
+    for line in lines_list:
+        print(line)
 
 
 
-get_file_lines(filename)
+
+file_lines = get_file_lines('poem.txt')
+lines_printed_backwards(file_lines)
+lines_printed_backwards
