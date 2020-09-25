3b4a378bdedabb9f34a9d3cf5458c222675c2eac
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-25 15:14:38 -0700

    print lines random

--- functions.py
+++ functions.py
@@ -3,23 +3,24 @@ import random
 def get_file_lines(filename):
     in_file = open(filename, "r")
     poem = in_file.readlines()
-    # print(poem)
     return poem
 
 def lines_printed_backwards(lines_list):
-    # get_file_lines('poem.txt')
     total_lines = len(lines_list) 
     for line in reversed(lines_list):
-        print(f'{total_lines} {line}')
+        # print(f'{total_lines} {line}')
         total_lines -= 1
 
 def lines_printed_random(lines_list):
-    for line in lines_list:
-        print(line)
-
-heloo
+    i = 0
+    while i < len(lines_list):
+        random_index = random.randint(0,42)
+        print(lines_list[random_index])
+        i += 1
 
 
 file_lines = get_file_lines('poem.txt')
+
 lines_printed_backwards(file_lines)
-lines_printed_backwards
+
+lines_printed_random(file_lines)
